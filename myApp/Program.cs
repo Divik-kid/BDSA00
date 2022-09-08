7955174e23f9bd798fa0851f63a3f159e365eb79
Chris <christopher.cortsen@gmail.com>
2022-09-08 22:32:09 +0200

    handling exceptions

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -3,17 +3,18 @@ public class Program{
 
     public static void Main(string[] arg)
     {
-        Console.WriteLine("Enter a year and we will say if it is a leap Year!");
+        Console.WriteLine("Enter a year after 1582 and we will say if it is a leap Year!");
         var input = Console.ReadLine();
         if(input == null){
              return;
         }
-        int yr = Int32.Parse(input);
+        
+        if(int.TryParse(input, out int value)){
+            int yr = Int32.Parse(input);
 
-        if(isLeapYear(yr)){
-            Console.WriteLine("yay");
+            answer(yr);
         }else{
-            Console.WriteLine("nay");
+            notYear();
         }
         
     }
@@ -23,6 +24,36 @@ public class Program{
     return true;
     }
     return false;
+}
+
+    public static void notYear()
+{   
+    bool breaker = false;
+
+    while(!breaker){
+        Console.WriteLine("Please enter a valid number above 1582");
+        var input = Console.ReadLine();
+        
+        if(input == null){
+             return;
+        }
+
+        if(int.TryParse(input, out int v)){
+            int mon = Int32.Parse(input);
+            if(mon > 1582){
+            breaker = true;
+            answer(v);  
+            }
+        }
+    }
+}
+
+public static void answer(int num){
+    if(isLeapYear(num)){
+            Console.WriteLine("yay");
+            }else{
+            Console.WriteLine("nay");
+            }
 }
 
 }
